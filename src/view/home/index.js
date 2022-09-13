2c21305eb8e06a7acba5eb293f837598d823ed6d
Danilo <cdanilocarvalho@outlook.com>
2022-09-12 21:51:43 -0300

    ajustes-home-2

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -2,23 +2,23 @@ import React, { useState, useEffect} from 'react';
 import './home.css';
 import Navbar from '../../components/navbar';
 import {useSelector} from 'react-redux';
-import Firebase from '../../config/firebase';
+import firebase from '../../config/firebase';
 import EventoCard from '../../components/evento-card/';
 
 function Home({match}){
 
-    const [eventos, setEventos] = useState(['']);
+    const [eventos, setEventos] = useState([]);
     const [pesquisa, setPesquisa] = useState('');
-    const [img, setImg] = useState('');
     let listaeventos = [];
     const usuarioEmail = useSelector(state => state.usuarioEmail);
 
     useEffect(() => {
-     
-        if(match.params.parametro){
-            Firebase.firestore().collection('Eventos').where('usuario','==',usuarioEmail).get().then(async (resultado) => {
+    
+    if(match.params.parametro){
+
+        firebase.firestore().collection('Eventos').where('usuario','==',usuarioEmail).get().then(async (resultado) => {
             await resultado.docs.forEach(doc =>{
-                if(doc.data().titulo.indexOf(pesquisa) >= 0)
+                if(doc.data().titulo.toUpperCase().indexOf(pesquisa.toUpperCase()) >= 0)
                 {
                     listaeventos.push({
                         id: doc.id,
@@ -26,42 +26,48 @@ function Home({match}){
                     })
                 }
             })
+            
             setEventos(listaeventos);
         });
 
-        }else{
-            Firebase.firestore().collection('Eventos').get().then(async (resultado) => {
-                await resultado.docs.forEach(doc =>{
-                    if(doc.data().titulo.indexOf(pesquisa) >= 0)
-                    {
-                        listaeventos.push({
-                            id: doc.id,
-                            ...doc.data()
-                        })
-                    }
-                })
-                console.log(eventos.map(item => item.id))
-                setEventos(listaeventos);
-            });
-        }
- 
-    },[]);
+    }else{
+
+        firebase.firestore().collection('Eventos').get().then(async (resultado) => {
+            await resultado.docs.forEach(doc =>{
+                if(doc.data().titulo.toUpperCase().indexOf(pesquisa.toUpperCase()) >= 0)
+                {
+                    listaeventos.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
+                }
+            })
+
+            setEventos(listaeventos);
+        });
+    }
+
+    
+        
+
+       
+    });
 
     return(
         <div className='main-home'>
             <Navbar/>    
+
             <div className="row p-3">
-                <h3 className="text-center p-5">Publicações</h3>
-                <input onChange={(e) => setPesquisa(e.target.value)} type="text" className="barra-pesquisa form-control text-center" placeholder="Pesquisar evento..."/>
+                <h3 className="text-center p-5 ">Publicações</h3>
+                <div class="input-group mb-3 barra-pesquisa">
+                    <input onChange={(e) => setPesquisa(e.target.value)} type="text" className="form-control text-center " placeholder="Pesquisar..."/>
+                    <span class="input-group-text"><i class="fa fa-search"></i></span>
+                </div>
             </div>
 
-            {
-                img ? <div className="row  mb-3 "><div class="spinner-border text-danger mx-auto mb-2"></div></div>
-                : 
-                <div className="row p-3 mt-3">
-                    {eventos.map (item => <EventoCard key={item.id} id={item.id} img={item.foto} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes}/>) }
-                </div>   
-            }    
+            <div className="row p-3">
+                {eventos.map (item => <EventoCard key={item.id} id={item.id} img={item.foto} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes}/>) }
+            </div>       
         </div>
     )
 };
